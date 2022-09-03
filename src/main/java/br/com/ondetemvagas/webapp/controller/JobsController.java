bbfda3266424749c6b8bc2eda6fe8eb62b38f435
Ricardo Campos <ricardo.campos@encora.com>
2022-09-03 19:06:31 -0300

    feat: create empty class JobsController

---
+++ src/main/java/br/com/ondetemvagas/webapp/controller/JobsController.java
@@ -0,0 +1,11 @@
+package br.com.ondetemvagas.webapp.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/jobs")
+public class JobsController {
+
+  // Nothing yet
+}
