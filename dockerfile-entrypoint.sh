450744b9b339b1954dcc909f3e92ebad9df2847e
Ricardo Campos <ricardo.campos@encora.com>
2022-09-06 17:41:01 -0300

    feat: remove env files from docker

--- dockerfile-entrypoint.sh
+++ dockerfile-entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 DEBUG_PRINT=`$DEBUG_MODE -eq true && echo $JAVA_DEBUG_OPS || echo ""`
-export $(cat /usr/share/service/.env | xargs)
 
 java \
     -Djava.security.egd=file:/dev/./urandom \
